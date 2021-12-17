ac3e6c89afa3309a18aff295fd7be6bfdf435cc9
Dalton Bunker <dbbunker3@gmail.com>
2021-12-17 09:44:28 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header({ title, buttonText }) {
+    return (
+        <div className='Header' style={{position: 'relative', color: 'white', backgroundColor: '#4c6dcd', marginTop: 0, padding: 10, display: 'flex', alignItems: 'center', justifyContent: 'space-evenly'}}>
+            <button style={{left: 50, height: 40, position: 'absolute', backgroundColor: 'white', fontWeight: 'bold'}}>{buttonText}</button>
+            <h2 style={{color: 'white'}}>{title}</h2>
+        </div>
+    )
+}
+
+export default Header
