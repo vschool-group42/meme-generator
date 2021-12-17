d98d1623995410260dde457bf3e4c7a1c6f83ff7
Dalton Bunker <dbbunker3@gmail.com>
2021-12-17 10:37:36 -0700

    updated styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import '../styles/Header.css'
 
 function Header({ title, buttonText, clickEvent }) {
     return (
-        <div className='Header' style={{position: 'relative', color: 'white', backgroundColor: '#4c6dcd', marginTop: 0, padding: 10, display: 'flex', alignItems: 'center', justifyContent: 'space-evenly'}}>
-            <button onClick={clickEvent} style={{left: 50, height: 40, position: 'absolute', backgroundColor: 'white', fontWeight: 'bold'}}>{buttonText}</button>
-            <h2 style={{color: 'white'}}>{title}</h2>
+        <div className='Header'>
+            <button onClick={clickEvent}>{buttonText}</button>
+            <div className='title'>{title}</div>
         </div>
     )
 }
